e8968a60ca5957b10914793431dfe838bca6adc0
Aleksey <natallia.mansheva@gmail.com>
2022-07-21 15:29:04 +0300

    add Java debugger adn run

--- practice_temp/ex001.java
+++ practice_temp/ex001.java
@@ -1,12 +1,12 @@
 public class ex001 {
     public static void main(String [] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.printf("x0 = ");
         int x0 = sc.nextInt();
         System.out.printf("x1 = ");
         int x1 = sc.nextInt();
         double d = Math.abs( x1 - x0 );
         System.out.println(d);
-        scanner.close();
+        sc.close();
     }
 }
